70fa601095454a44f2c9b4f0614d082c5dc5f668
Mattia Dalzocchio <mattiadalzocchio@me.com>
2025-05-31 16:24:23 +0200

    feat: export schemas

--- src/schemas/mantaray-schema.ts
+++ src/schemas/mantaray-schema.ts
@@ -1,14 +1,14 @@
 import { z } from "zod/v4"
 
-export const MantarayNodeSchema: z.ZodSchema<MantarayNode> = z.lazy(() =>
-  z.object({
+export const MantarayNodeSchema: z.ZodSchema<MantarayNode> = z.lazy(() => {
+  return z.object({
     type: z.number(),
     entry: z.string().optional(),
     contentAddress: z.string().optional(),
     metadata: z.record(z.string(), z.string()).optional(),
     forks: z.record(z.string(), MantarayForkSchema),
-  }),
-)
+  })
+})
 
 export const MantarayForkSchema = z.object({
   prefix: z.string(),
@@ -16,11 +16,11 @@ export const MantarayForkSchema = z.object({
 })
 
 // Types
-export type MantarayNode = {
+type MantarayNode = {
   type: number
   entry?: string
   contentAddress?: string
   metadata?: Record<string, string>
-  forks: Record<string, MantarayFork>
+  forks: Record<string, MantarayNodeFork>
 }
-export type MantarayFork = z.infer<typeof MantarayForkSchema>
+type MantarayNodeFork = z.infer<typeof MantarayForkSchema>
